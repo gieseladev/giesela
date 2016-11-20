e725af8a38a7e738d19b11ccf49360bf8bba68c6
Simon Berger <siku2@outlook.de>
2016-11-20 23:18:08 +0100

    Finished the basic playlist features
    still have to implement the builder.

--- musicbot/saved_playlists.py
+++ musicbot/saved_playlists.py
@@ -10,6 +10,7 @@ class Playlists:
     def __init__(self, playlists_file):
         self.playlists_file = playlists_file
         self.update_playlist ()
+        self.playlist_save_location = "data/playlists/"
 
     def update_playlist (self):
         self.playlists = configparser.ConfigParser()
@@ -23,6 +24,10 @@ class Playlists:
         self.playlists = configparser.ConfigParser(interpolation=None)
         self.playlists.read (self.playlists_file, encoding='utf-8')
         self.saved_playlists = self.playlists.sections ()
+
+    def save_playlist (self):
+        with open (self.playlists_file, "w") as pl_file:
+            self.playlists.write (pl_file)
 
     def get_playlist (self, playlistname):
         if not self.playlists.has_section (playlistname):
@@ -40,3 +45,27 @@ class Playlists:
         playlist_informations ["entries"] = entries
 
         return playlist_informations
+
+    def set_playlist (self, entries, name, author_id):
+        try:
+            with open (self.playlist_save_location + str (name) + ".txt", "w") as f:
+                for entry in entries:
+                    f.write (entry + "\n")
+        except Exception as e:
+            raise ValueError (str (e))
+            return False
+
+        self.playlists.add_section (name)
+        self.playlists.set (name, "location", self.playlist_save_location + str (name) + ".txt")
+        self.playlists.set (name, "author", str (author_id))
+        self.playlists.set (name, "entries", str (len (entries)))
+
+        self.save_playlist ()
+        self.update_playlist ()
+        return True
+
+    def remove_playlist (self, name):
+        os.remove (self.playlists [name] ["location"])
+        self.playlists.remove_section (name)
+        self.save_playlist ()
+        self.update_playlist ()
