3e6688029ad73f103d423421999d6635976b6d47
Simon Berger <siku2@outlook.de>
2016-12-06 17:27:50 +0100

    Fixed a huge playlist bug where you couldn't add any playlists or videos by url
    (Because the url was lowered)

--- musicbot/playlist.py
+++ musicbot/playlist.py
@@ -190,6 +190,137 @@ class Playlist(EventEmitter):
             except:
                 pass
 
+    async def entries_import_from(self, playlist_url, **meta):
+        """
+            Imports the songs from `playlist_url` and queues them to be played.
+
+            Returns a list of `entries` that have been enqueued.
+
+            :param playlist_url: The playlist url to be cut into individual urls and added to the playlist
+            :param meta: Any additional metadata to add to the playlist entry
+        """
+        entry_list = []
+
+        try:
+            info = await self.downloader.safe_extract_info(self.loop, playlist_url, download=False)
+        except Exception as e:
+            raise ExtractionError('Could not extract information from {}\n\n{}'.format(playlist_url, e))
+
+        if not info:
+            raise ExtractionError('Could not extract information from %s' % playlist_url)
+
+        # Once again, the generic extractor fucks things up.
+        if info.get('extractor', None) == 'generic':
+            url_field = 'url'
+        else:
+            url_field = 'webpage_url'
+
+        baditems = 0
+        for items in info['entries']:
+            if items:
+                try:
+                    entry = URLPlaylistEntry(
+                        self,
+                        items[url_field],
+                        items.get('title', 'Untitled'),
+                        items.get('duration', 0) or 0,
+                        self.downloader.ytdl.prepare_filename(items),
+                        **meta
+                    )
+                    entry_list.append(entry)
+                except:
+                    baditems += 1
+                    # Once I know more about what's happening here I can add a proper message
+                    traceback.print_exc()
+                    print(items)
+                    print("Could not add item")
+            else:
+                baditems += 1
+
+        if baditems:
+            print("Skipped %s bad entries" % baditems)
+
+        return entry_list
+
+    async def entries_async_process_youtube_playlist(self, playlist_url, **meta):
+        """
+            Processes youtube playlists links from `playlist_url` in a questionable, async fashion.
+
+            :param playlist_url: The playlist url to be cut into individual urls and added to the playlist
+            :param meta: Any additional metadata to add to the playlist entry
+        """
+
+        try:
+            info = await self.downloader.safe_extract_info(self.loop, playlist_url, download=False, process=False)
+        except Exception as e:
+            raise ExtractionError('Could not extract information from {}\n\n{}'.format(playlist_url, e))
+
+        if not info:
+            raise ExtractionError('Could not extract information from %s' % playlist_url)
+
+        gooditems = []
+        baditems = 0
+        for entry_data in info['entries']:
+            if entry_data:
+                baseurl = info['webpage_url'].split('playlist?list=')[0]
+                song_url = baseurl + 'watch?v=%s' % entry_data['id']
+
+                try:
+                    entry = await self.get_entry(song_url, **meta)
+                    gooditems.append(entry)
+                except ExtractionError:
+                    baditems += 1
+                except Exception as e:
+                    baditems += 1
+                    print("There was an error adding the song {}: {}: {}\n".format(
+                        entry_data['id'], e.__class__.__name__, e))
+            else:
+                baditems += 1
+
+        if baditems:
+            print("Skipped %s bad entries" % baditems)
+
+        return gooditems
+
+    async def entries_async_process_sc_bc_playlist(self, playlist_url, **meta):
+        """
+            Processes soundcloud set and bancdamp album links from `playlist_url` in a questionable, async fashion.
+
+            :param playlist_url: The playlist url to be cut into individual urls and added to the playlist
+            :param meta: Any additional metadata to add to the playlist entry
+        """
+
+        try:
+            info = await self.downloader.safe_extract_info(self.loop, playlist_url, download=False, process=False)
+        except Exception as e:
+            raise ExtractionError('Could not extract information from {}\n\n{}'.format(playlist_url, e))
+
+        if not info:
+            raise ExtractionError('Could not extract information from %s' % playlist_url)
+
+        gooditems = []
+        baditems = 0
+        for entry_data in info['entries']:
+            if entry_data:
+                song_url = entry_data['url']
+
+                try:
+                    entry = await self.get_entry(song_url, **meta)
+                    gooditems.append(entry)
+                except ExtractionError:
+                    baditems += 1
+                except Exception as e:
+                    baditems += 1
+                    print("There was an error adding the song {}: {}: {}\n".format(
+                        entry_data['id'], e.__class__.__name__, e))
+            else:
+                baditems += 1
+
+        if baditems:
+            print("Skipped %s bad entries" % baditems)
+
+        return gooditems
+
     async def import_from(self, playlist_url, **meta):
         """
             Imports the songs from `playlist_url` and queues them to be played.
@@ -244,54 +375,6 @@ class Playlist(EventEmitter):
             print("Skipped %s bad entries" % baditems)
 
         return entry_list, position
-
-    async def get_playlist_entries(self, playlist_url, **meta):
-        entry_list = []
-
-        try:
-            info = await self.downloader.safe_extract_info(self.loop, playlist_url, download=False)
-        except Exception as e:
-            raise ExtractionError('Could not extract information from {}\n\n{}'.format(playlist_url, e))
-
-        if not info:
-            raise ExtractionError('Could not extract information from %s' % playlist_url)
-
-        # Once again, the generic extractor fucks things up.
-        if info.get('extractor', None) == 'generic':
-            url_field = 'url'
-        else:
-            url_field = 'webpage_url'
-
-        if "entries" not in info:
-            return None
-
-        baditems = 0
-        for items in info['entries']:
-            if items:
-                try:
-                    entry = URLPlaylistEntry(
-                        self,
-                        items[url_field],
-                        items.get('title', 'Untitled'),
-                        items.get('duration', 0) or 0,
-                        self.downloader.ytdl.prepare_filename(items),
-                        **meta
-                    )
-
-                    entry_list.append(entry)
-                except:
-                    baditems += 1
-                    # Once I know more about what's happening here I can add a proper message
-                    traceback.print_exc()
-                    print(items)
-                    print("Could not add item")
-            else:
-                baditems += 1
-
-        if baditems:
-            print("Skipped %s bad entries" % baditems)
-
-        return entry_list
 
     async def async_process_youtube_playlist(self, playlist_url, **meta):
         """
