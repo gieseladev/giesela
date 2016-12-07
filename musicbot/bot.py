569c4d9ff05c2bff8d3fe8be17b4b79d6a8ac1c6
Simon Berger <siku2@outlook.de>
2016-12-07 19:48:58 +0100

    Actually using the rounding feature nwo

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2591,7 +2591,7 @@ class MusicBot(discord.Client):
             print (str (game_list))
             for game in game_list:
                 pass
-                
+
             return
 
         game = game.lower ()
@@ -2957,7 +2957,7 @@ class MusicBot(discord.Client):
 
             for pl in self.playlists.saved_playlists:
                 infos = self.playlists.get_playlist (pl, player.playlist)
-                response_text += "  {}. \"{}\" added by *{}* with {} entr{} and a playtime of {}\n".format (iteration, pl.title (), server.get_member (infos ["author"]).mention, str (infos ["entry_count"]), "ies" if int (infos ["entry_count"]) is not 1 else "y", format_time (sum ([x.duration for x in infos ["entries"]])))
+                response_text += "  {}. \"{}\" added by *{}* with {} entr{} and a playtime of {}\n".format (iteration, pl.title (), server.get_member (infos ["author"]).mention, str (infos ["entry_count"]), "ies" if int (infos ["entry_count"]) is not 1 else "y", format_time (sum ([x.duration for x in infos ["entries"]]), round_seconds = True, max_specifications = 2))
                 iteration += 1
 
             #self.safe_print (response_text)
