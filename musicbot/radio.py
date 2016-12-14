a0b3627b901fc12ae697dd7085d698b4ed99eea2
Simon Berger <siku2@outlook.de>
2016-12-14 21:39:30 +0100

    Antoher fix I guess. I think it should work now, but I'm not sure...

--- musicbot/radio.py
+++ musicbot/radio.py
@@ -16,29 +16,37 @@ class Radio:
             soup = BeautifulSoup(html, "lxml")
             tags = soup.find_all('div', class_="song_wrapper", limit=10)
             for t in tags:
-                name = t.find("a", class_="track") or t.find("span", class_="track")
-                author = t.find("a", class_="first") or t.find("span", class_="artist")
+                name = t.find("a", class_="track") or t.find(
+                    "span", class_="track")
+                author = t.find("a", class_="first") or t.find(
+                    "span", class_="artist")
                 if name is not None:
                     information = (name.text.strip(), author.text.strip()
                                    if author is not None else "Unknown")
                     if (information not in self.song_list and information not in self.old_songs) or (replay_songs and information not in self.song_list):
                         self.song_list.append(information)
 
-    def update_list (self, replay_songs=False):
-            self.song_list = []
+    def update_list(self, replay_songs=False):
+        self.song_list = []
 
-            self.update_list_capitalfm (replay_songs)
+        self.update_list_capitalfm(replay_songs)
+
+    def refresh(self):
+        self.song_list = []
+        self.old_songs = []
 
     def get_next_song(self, update=True, replay_songs=True):
         if len(self.song_list) < 1 or update:
             self.update_list()
 
             if len(self.song_list) < 1 and replay_songs:
-                self.update_list(True)
+                self.update_list(replay_songs=True)
 
                 if len(self.song_list) < 1 and len(self.old_songs) > 0:
                     return self.old_songs.pop()
                 else:
+                    print(
+                        "Is the radio station offline or something? I don't have any kind of entries...")
                     return None
 
         song = self.song_list.pop(0)
