c73fbf3c6a0b065633cea6fac757935e8c875dca
Simon Berger <siku2@outlook.de>
2017-01-05 16:36:56 +0100

    getting there. There are not many exceptions left

--- musicbot/spotify.py
+++ musicbot/spotify.py
@@ -6,7 +6,7 @@ import spotipy
 
 class SpotifyTrack:
 
-    def __init__(self, artist, song_name, cover_url, certainty=1):
+    def __init__(self, artist, song_name, cover_url, query, certainty=1):
         self.artist = artist
         self.song_name = song_name
         self.cover_url = cover_url
@@ -19,18 +19,20 @@ class SpotifyTrack:
 
         query = re.sub("'", "", query)
 
-        index = query.find("|")
-        query = query[:index if index > 3 else len(query)]
+        query = query.replace("|", " ", 1)
 
         index = query.lower().find("download")
         query = query[:index if index > 3 else len(query)]
 
-        index = query.lower().find("ft")
+        index = query.lower().find(" ft")
         query = query[:index if index > 3 else len(query)]
         index = query.lower().find("feat")
         query = query[:index if index > 3 else len(query)]
 
         index = query.lower().find("lyric")
+        query = query[:index if index > 3 else len(query)]
+
+        index = query.lower().find("official")
         query = query[:index if index > 3 else len(query)]
 
         index = query.lower().find("&") if query.lower().find(
@@ -50,9 +52,9 @@ class SpotifyTrack:
         spotify = spotipy.Spotify()
         search_result = spotify.search(query, limit=1, type="track")
         if len(search_result) < 1:
-            return cls("", query.upper(), "", 0)
+            return cls("", query.upper(), "", query, 0)
         if len(search_result["tracks"]["items"]) < 1:
-            return cls("", query.upper(), "", 0)
+            return cls("", query.upper(), "", query, 0)
 
         track = search_result["tracks"]["items"][0]
         try:
@@ -68,7 +70,12 @@ class SpotifyTrack:
         song_name_edited = re.sub(
             "\***REMOVED***0[0]***REMOVED***.+\***REMOVED***0[1]***REMOVED***".format("()"), "", song_name)
 
-        return cls(artist_text.upper(), song_name.upper(), cover, max(similar(query, "***REMOVED***0***REMOVED*** ***REMOVED***1***REMOVED***".format(artists[0]["name"], song_name_edited)), similar(query, "***REMOVED***1***REMOVED*** ***REMOVED***0***REMOVED***".format(artists[0]["name"], song_name_edited))))
+        index = song_name_edited.find("-")
+        song_name_edited = song_name_edited[
+            :index if index > 3 else len(song_name_edited)]
+        song_name_edited = song_name_edited.strip()
+
+        return cls(artist_text.upper(), song_name.upper(), cover, query, max(similar(query.lower(), "***REMOVED***0***REMOVED*** ***REMOVED***1***REMOVED***".format(artists[0]["name"].lower(), song_name_edited.lower())), similar(query.lower(), "***REMOVED***1***REMOVED*** ***REMOVED***0***REMOVED***".format(artists[0]["name"].lower(), song_name_edited.lower()))))
 
 
 def similar(a, b):
