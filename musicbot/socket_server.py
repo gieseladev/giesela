bef7a75179935c10dc31cb96080ff1dce765e4cf
Simon Berger <siku2@outlook.de>
2017-01-05 09:35:01 +0100

    Pretty much done now

--- musicbot/socket_server.py
+++ musicbot/socket_server.py
@@ -2,6 +2,8 @@ import time
 from socket import *
 from threading import Thread
 
+from .spotify import SpotifyTrack
+
 
 class SocketServer:
 
@@ -13,6 +15,7 @@ class SocketServer:
         self.buf_size = 1024
         self.max_connections = 10
         self.connections = []
+        self.server_ids = ***REMOVED******REMOVED***
         self.stop_threads = False
 
         main_socket = socket(AF_INET, SOCK_STREAM)
@@ -27,6 +30,23 @@ class SocketServer:
         self.main_socket.shutdown(SHUT_RDWR)
         self.main_socket.close()
         self.stop_threads = True
+
+    def threaded_broadcast_information(self):
+        work_thread = Thread(target=self._broadcast_information)
+        work_thread.start()
+
+    def _broadcast_information(self):
+        for sock, server_id in self.server_ids.items():
+            response = "INFORMATION;***REMOVED***artist***REMOVED***;***REMOVED***song_title***REMOVED***;***REMOVED***play_status***REMOVED***;***REMOVED***cover_url***REMOVED***;***REMOVED***progress***REMOVED***;***REMOVED***duration***REMOVED***;***REMOVED***volume***REMOVED***"
+
+            artist, song_title, cover_url, playing, duration, progress, volume = self.get_player_values(
+                server_id)
+
+            response = response.format(artist=artist, song_title=song_title, play_status=playing,
+                                       cover_url=cover_url, progress=progress, duration=duration, volume=volume)
+            #print("I sent\n\n***REMOVED******REMOVED***\n\n========".format(response))
+            print("[SOCKETSERVER] Broadcasted information")
+            sock.send(response.encode("utf-8"))
 
     def connection_accepter(self):
         while not self.stop_threads:
@@ -61,6 +81,7 @@ class SocketServer:
                 parts = msg.split(";")
                 request = parts[0]
                 server_id = parts[1]
+                self.server_ids[c_socket] = server_id
                 leftover = parts[2:]
             except:
                 print("[SOCKETSERVER] Socket sent malformed message")
@@ -68,21 +89,12 @@ class SocketServer:
 
             if request == "REQUEST" and leftover[0] == "SEND_INFORMATION":
                 response = "INFORMATION;***REMOVED***artist***REMOVED***;***REMOVED***song_title***REMOVED***;***REMOVED***play_status***REMOVED***;***REMOVED***cover_url***REMOVED***;***REMOVED***progress***REMOVED***;***REMOVED***duration***REMOVED***;***REMOVED***volume***REMOVED***"
-                if server_id in self.musicbot.players:
-                    player = self.musicbot.players[server_id]
-                    if player.current_entry is None:
-                        playing = "STOPPED"
-                        duration = "0"
-                        progress = "0"
-                    else:
-                        playing = "PLAYING" if player.is_playing else "PAUSED"
-                        duration = str(player.current_entry.duration)
-                        progress = str(player.progress)
 
-                    volume = player.volume
+                artist, song_title, cover_url, playing, duration, progress, volume = self.get_player_values(
+                    server_id)
 
-                response = response.format(artist="AVICII", song_title="FOR A BETTER DAY", play_status=playing,
-                                           cover_url="https://i.scdn.co/image/1e95e13d082e43c547dadd93808dceeb99f589cf", progress=progress, duration=duration, volume=volume)
+                response = response.format(artist=artist, song_title=song_title, play_status=playing,
+                                           cover_url=cover_url, progress=progress, duration=duration, volume=volume)
                 print("[SOCKETSERVER] Socket sent data")
                 c_socket.send(response.encode("utf-8"))
             elif request == "COMMAND":
@@ -102,6 +114,8 @@ class SocketServer:
                     elif leftover[0] == "VOLUMECHANGE":
                         player.volume = float(leftover[1])
 
+                    self.threaded_broadcast_information()
+
         to_delete = None
         for i in range(len(self.connections)):
             if self.connections[i][1] == c_socket:
@@ -116,3 +130,33 @@ class SocketServer:
 
         c_socket.shutdown(SHUT_RDWR)
         c_socket.close()
+
+    def get_player_values(self, server_id):
+        if server_id in self.musicbot.players:
+            player = self.musicbot.players[server_id]
+            if player.current_entry is None:
+                artist = " "
+                song_title = "None"
+                cover_url = "http://i.imgur.com/nszu54A.jpg"
+                playing = "STOPPED"
+                duration = "0"
+                progress = "0"
+            else:
+                spotify_track = SpotifyTrack.from_query(
+                    player.current_entry.title)
+                if spotify_track.certainty > .6:
+                    artist = spotify_track.artist
+                    song_title = spotify_track.song_name
+                    cover_url = spotify_track.cover_url
+                else:
+                    artist = " "
+                    song_title = player.current_entry.title
+                    cover_url = "http://i.imgur.com/nszu54A.jpg"
+
+                playing = "PLAYING" if player.is_playing else "PAUSED"
+                duration = str(player.current_entry.duration)
+                progress = str(player.progress)
+
+            volume = player.volume
+
+        return artist, song_title, cover_url, playing, duration, progress, volume
