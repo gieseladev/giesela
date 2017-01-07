4ccbf74927e85c60b42e6750205b7257e1845db2
Simon Berger <siku2@outlook.de>
2017-01-07 20:59:46 +0100

    another query parser update

--- musicbot/socket_server.py
+++ musicbot/socket_server.py
@@ -201,7 +201,7 @@ class SocketServer:
             volume = str(round(player.volume, 2))
         else:
             artist = " "
-            song_title = "NOT PLAYING"
+            song_title = "NOT CONNECTED TO A CHANNEL"
             cover_url = "http://i.imgur.com/nszu54A.jpg"
             playing = "STOPPED"
             duration = "0"
