aff3610a8eb4022f11c224415dbe997c824ec21f
Simon Berger <siku2@outlook.de>
2017-01-12 21:54:19 +0100

    I think I fixed something

--- musicbot/socket_server.py
+++ musicbot/socket_server.py
@@ -47,11 +47,10 @@ class SocketServer:
                 sck = sock
                 break
 
-        self.awaiting_registeration.pop(sck)
-
         if sck is None:
             return False
         else:
+            self.awaiting_registeration.pop(sck)
             response = "USERINFORMATION;***REMOVED******REMOVED***;***REMOVED******REMOVED***".format(server_id, author_id)
             sck.sendall("***REMOVED******REMOVED***==***REMOVED******REMOVED***".format(
                 len(response), response).encode("utf-8"))
