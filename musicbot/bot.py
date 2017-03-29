2b5b2ac850e1446567b57de0674fa0b130f9b5e4
Simon Berger <siku2@outlook.de>
2017-03-29 22:33:07 +0200

    another lil tweak

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2613,14 +2613,14 @@ class MusicBot(discord.Client):
         cbOne = CleverWrap("CCC8n_IXK43aOV38rcWUILmYUBQ")
         cbTwo = CleverWrap("CCC8n_IXK43aOV38rcWUILmYUBQ")
         answer = cbOne.say(choice["hey", "salut", "hallo", "hello", "hi there", "hello there", "good evening"])
-        await self.safe_send_message(channel, "Bot #1: Hello there")
+        await self.safe_send_message(channel, "Regi: Hello there")
 
         while self.newLonelyState:
             await asyncio.sleep(1.8)
-            await self.safe_send_message(channel, 'Bot #2: ***REMOVED******REMOVED***'.format(answer))
+            await self.safe_send_message(channel, 'Giesela: ***REMOVED******REMOVED***'.format(answer))
             answer = cbOne.say(answer)
             await asyncio.sleep(1.8)
-            await self.safe_send_message(channel, 'Bot #1: ***REMOVED******REMOVED***'.format(answer))
+            await self.safe_send_message(channel, 'Regi: ***REMOVED******REMOVED***'.format(answer))
             answer = cbTwo.say(answer)
 
     async def cmd_random(self, channel, author, leftover_args):
