5c7b1c315ad7912eb58fff67af1e1b761936ebcc
Simon Berger <siku2@outlook.de>
2017-04-19 19:17:52 +0200

    2048 save code is correct now

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -3652,7 +3652,7 @@ class MusicBot(discord.Client):
                         turn_information = "| *replay has been sent*"
 
                     if str(reaction.emoji) == "💾" and reaction.count > 1:
-                        await self.safe_send_message(user, "The save code is: *{0}*\nUse `{1}game 2048 {0}` to continue your current game".format(escape_dis(game.get_save()), self.config.command_prefix))
+                        await self.safe_send_message(user, "The save code is: *{0}*\nUse `{1}game 2048 {2}` to continue your current game".format(escape_dis(game.get_save()), self.config.command_prefix, game.get_save()))
                         turn_information = "| *save code has been sent*"
 
                     if str(reaction.emoji) in ("⬇", "➡", "⬆", "⬅") and reaction.count > 1:
@@ -3664,7 +3664,7 @@ class MusicBot(discord.Client):
 
                 if direction is None:
                     await self.safe_delete_message(msg)
-                    turn_information = "| *You didn't specifiy the direction*"
+                    turn_information = "| *You didn't specifiy the direction*" if turn_information is not "" else turn_information
 
             # self.log ("Chose the direction " + str (direction))
             game.move(direction)
