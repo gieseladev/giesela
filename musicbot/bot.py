8dafad976ae1ac64430a4f6d3eff237cfb4f0ee7
Simon Berger <siku2@outlook.de>
2017-04-23 14:50:19 +0200

    working on the help function (using embeds)

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -894,12 +894,11 @@ class MusicBot(discord.Client):
 
     async def cmd_help(self, channel, leftover_args):
         """
-        Usage:
-            {command_prefix}help [command]
+        ///|Usage
+        `{command_prefix}help [command]`
 
-        logs a help message.
-        If a command is specified, it logs a help message for that command.
-        Otherwise, it lists the available commands.
+        ///|Explanation
+        Logs a help message.
         """
         command = None
 
@@ -909,13 +908,33 @@ class MusicBot(discord.Client):
         if command:
             cmd = getattr(self, 'cmd_' + command, None)
             if cmd:
-                return Response(
-                    "```\n{}```".format(
-                        dedent(cmd.__doc__).format(
-                            command_prefix=self.config.command_prefix)
-                    ),
-                    delete_after=60
-                )
+                documentation = cmd.__doc__.format(
+                    command_prefix=self.config.command_prefix)
+                em = Embed(title="**{}**".format(command.upper()))
+                fields = documentation.split("///")
+                if len(fields) < 1:  # backward compatibility
+                    return Response("```\n{}```".format(dedent(cmd.__doc__).format(command_prefix=self.config.command_prefix)), delete_after=60)
+
+                for field in fields:
+                    if field is None or field is "":
+                        continue
+                    inline = True
+                    if field.startswith("(NL)"):
+                        inline = False
+                        field = field[4:]
+                        # print(field)
+
+                    match = re.match("\|(.+)\n((?:.|\n)+)", field)
+                    if match is None:
+                        continue
+                    title, text = match.group(1, 2)
+
+                    em.add_field(name="**{}**".format(title),
+                                 value=text, inline=inline)
+                await self.send_message(channel, embed=em)
+                return
+                # return
+                # Response("```\n{}```".format(dedent(cmd.__doc__).format(command_prefix=self.config.command_prefix)),delete_after=60)
             else:
                 # return Response("No such command", delete_after=10)
                 self.log("Didn't find a command like that")
@@ -971,11 +990,10 @@ class MusicBot(discord.Client):
 
     async def cmd_blacklist(self, message, user_mentions, option, something):
         """
-        Usage:
-            {command_prefix}blacklist [ + | - | add | remove ] @UserName [@UserName2 ...]
-
+        ///|Usage
+        {command_prefix}blacklist [ + | - | add | remove ] @UserName [@UserName2 ...]
+        ///|Explanation
         Add or remove users to the blacklist.
-        Blacklisted users are forbidden from using bot commands.
         """
 
         if not user_mentions:
@@ -1021,9 +1039,9 @@ class MusicBot(discord.Client):
 
     async def cmd_id(self, author, user_mentions):
         """
-        Usage:
-            {command_prefix}id [@user]
-
+        ///|Usage
+        {command_prefix}id [@user]
+        ///|Explanation
         Tells the user their id or the id of another user.
         """
         if not user_mentions:
@@ -1750,9 +1768,9 @@ class MusicBot(discord.Client):
 
     async def cmd_np(self, player, channel, server, message):
         """
-        Usage:
-            {command_prefix}np
-
+        ///|Usage
+        {command_prefix}np
+        ///|Explanation
         Displays the current song in chat.
         """
 
@@ -3893,17 +3911,20 @@ class MusicBot(discord.Client):
 
     async def cmd_playlist(self, channel, author, server, player, leftover_args):
         """
-        Usage:
-            {command_prefix}playlist showall [alphabetical | author | entries | playtime | random | replays]
-            {command_prefix}playlist <savename>
-            {command_prefix}playlist save <savename>
-            {command_prefix}playlist load <savename> [add | replace] [none | alphabetical | length | random] [startindex | endindex (inclusive)]
-            {command_prefix}playlist delete <savename>
-            {command_prefix}playlist clone <fromname> <savename> [startindex | endindex (inclusive)]
-
-            {command_prefix}playlist builder <savename>
-
-        Save the current playlist so you can load it again later. Every savename has to be unique. Just typing the savename after the commands gives you some information of the playlist.
+        ///|Load
+        `{command_prefix}playlist load <savename> [add | replace] [none | alphabetical | length | random] [startindex | endindex (inclusive)]`
+        ///(NL)|List all playlists
+        `{command_prefix}playlist showall [alphabetical | author | entries | playtime | random | replays]`
+        ///(NL)|Build a new playlist
+        `{command_prefix}playlist builder <savename>`
+        ///(NL)|Save the current queue
+        `{command_prefix}playlist save <savename>`
+        ///(NL)|Clone
+        `{command_prefix}playlist clone <fromname> <savename> [startindex | endindex (inclusive)]`
+        ///(NL)|Delete a playlist
+        `{command_prefix}playlist delete <savename>`
+        ///(NL)|Information
+        `{command_prefix}playlist <savename>`
         """
 
         argument = leftover_args[0].lower() if len(leftover_args) > 0 else ""
