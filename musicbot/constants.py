84c1ebf76b3013f11f099f4ad6a3af09520e5fe7
Simon Berger <siku2@outlook.de>
2017-06-08 22:10:38 +0200

    writing the changelog

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -3,8 +3,8 @@ import re
 
 import requests
 
-MAIN_VERSION = '3.2.8'
-SUB_VERSION = 'GG_iesela'
+MAIN_VERSION = '3.3.0'
+SUB_VERSION = 'Gieselator'
 VERSION = MAIN_VERSION + "_" + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
