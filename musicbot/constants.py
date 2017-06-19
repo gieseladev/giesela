720b28fe7c2ec7d9b1345abf8613e882170fd88c
Simon <simon.siku@yahoo.de>
2017-06-19 20:15:56 +0100

    replay displaying song name and <code> html_to_markdown

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -47,8 +47,8 @@ def get_dev_changelog():
     changelog_page = requests.get(
         base_url + dev_version).content.decode("utf-8")
     bs = BeautifulSoup(changelog_page, "lxml")
-    html_to_markdown = [(r"<\/?li>", "\t"), (r"<\/?ul>", ""), (r"<strong>(.+?)<\/strong>",
-                                                               r"**\1**"), (r"<a.+?>(.+?)<\/a>", r"`\1`"), (r"\n\W+\n", "\n")]
+    html_to_markdown = [(r"<\/?li>", "\t"), (r"<\/?ul>", ""), (r"<code.+?>(.+?)<\/code>", r"`\1`"), (r"<strong>(.+?)<\/strong>",
+                                                                                                     r"**\1**"), (r"<a.+?>(.+?)<\/a>", r"`\1`"), (r"\n\W+\n", "\n")]
 
     changes = []
 
