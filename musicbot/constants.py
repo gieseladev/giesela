c4d4adaaa533b85c8ce3d926a2867bcbd7f07b60
Simon <simon.siku@yahoo.de>
2017-07-02 21:06:52 +0100

    autopause behaviour is much more to my likings

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -4,7 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-MAIN_VERSION = "3.7.8"
+MAIN_VERSION = "3.7.9"
 SUB_VERSION = "Giese_La_La_Land"
 VERSION = MAIN_VERSION + "_" + SUB_VERSION
 
