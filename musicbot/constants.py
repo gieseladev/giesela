5278090d967c8a6443b2da055694d7deea1a93f6
Simon Berger <siku2@outlook.de>
2017-07-23 16:17:33 +0200

    misc

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,14 +1,11 @@
 import os.path
-import re
-
-import requests
-from bs4 import BeautifulSoup
 
 MAIN_VERSION = "4.1.5"
 SUB_VERSION = "Gooma"
 VERSION = MAIN_VERSION + "_" + SUB_VERSION
 
 all_sub_versions = ***REMOVED***
+    "4.1.x": "Gooma",
     "4.0.x": "New_Reign",
     "3.9.x": "Giesenesis",
     "3.8.x": "Giezela",
@@ -19,7 +16,8 @@ all_sub_versions = ***REMOVED***
     "3.3.x": "Giselator",
     "3.2.x": "GG_iesela",
     "3.1.x": "age_of_Giesela",
-    "3.0.x": "Giesela-PLUS"***REMOVED***
+    "3.0.x": "Giesela-PLUS"
+***REMOVED***
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), "audio_cache")
 DISCORD_MSG_CHAR_LIMIT = 2000
