b5740139ed616e494fffd251fe7ecc91d6d5dd25
Simon Berger <siku2@outlook.de>
2017-07-25 09:09:49 +0200

    converting those fake SpotifyEntries to GieselaEntries

--- musicbot/entry_updater.py
+++ musicbot/entry_updater.py
@@ -1,4 +1,4 @@
-from musicbot.entry import Entry
+from musicbot.entry import Entry, GieselaEntry
 
 from .exceptions import ExtractionError, WrongEntryTypeError
 
@@ -24,14 +24,41 @@ async def _fix_filename(queue, entry):
     except (ExtractionError, WrongEntryTypeError):
         return None
 
+
+def _convert_spotify_to_giesela(queue, entry):
+    video_id = entry.get("video_id")
+    url = entry.get("url")
+    title = entry.get("title")
+    duration = entry.get("duration")
+    thumbnail = entry.get("thumbnail")
+    description = entry.get("description")
+    song_title = entry.get("song_title")
+    artist = entry.get("artist")
+    artist_image = entry.get("artist_image")
+    album = entry.get("album")
+    cover = entry.get("cover")
+    expected_filename = entry.get("expected_filename")
+    meta = entry.get("meta")
+
+    return GieselaEntry(queue, video_id, url, title, duration, thumbnail, description, song_title, artist, artist_image, album, cover, expected_filename=None, **meta).to_dict()
+
 async def fix_entry(queue, entry):
     version = entry.get("version", 0)
+
+    if entry.get("spotify_data", ***REMOVED******REMOVED***).get("id", None) == "custom":
+        entry = _convert_spotify_to_giesela(queue, entry)
 
     if version < 100:
         return await _rebuild_entry(queue, entry)
 
-    if between(version, 100, 101) and "expected_filename" not in entry:
+    if "expected_filename" not in entry:
         return await _fix_filename(queue, entry)
+
+    try:
+        entry["version"] = Entry.version
+        return Entry.from_dict(queue, entry)
+    except:
+        return await _rebuild_entry(queue, entry)
 
 async def fix_generator(queue, *entries):
     for ind, entry in enumerate(entries):
