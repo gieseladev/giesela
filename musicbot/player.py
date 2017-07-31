ae6ecb1121c3bd394eb0f9445e7810f8f87d461f
Simon Berger <siku2@outlook.de>
2017-07-31 15:01:55 +0200

    just making sure

--- musicbot/player.py
+++ musicbot/player.py
@@ -421,6 +421,8 @@ class MusicPlayer(EventEmitter):
                 elif isinstance(self.current_entry, RadioSongEntry):
                     if self.current_entry.song_duration > 5:
                         delay = self.current_entry.song_duration - self.current_entry.song_progress + 2
+                        if delay < 0:
+                            delay = 40
                     else:
                         delay = 40
                 else:
