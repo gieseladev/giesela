024dfb86141a94f41891c232a8aff229b4b846bd
Simon Berger <siku2@outlook.de>
2017-07-31 15:39:55 +0200

    another fix

---
+++ musicbot/web_author.py
@@ -0,0 +1,29 @@
+class WebAuthor:
+
+    def __init__(self, id, name, display_name, avatar_url, colour):
+        self.id = id
+        self.name = name
+        self.display_name = display_name
+        self.avatar_url = avatar_url
+        self.colour = colour
+
+    @classmethod
+    def from_id(cls, author_id):
+        user = GieselaServer.bot.get_global_user(author_id)
+        return cls(author_id, user.name, user.display_name, user.avatar_url, dec_to_hex(user.colour.value))
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+
+    def __str__(self):
+        return "[***REMOVED******REMOVED***/***REMOVED******REMOVED***]".format(self.id, self.name)
+
+    def to_dict(self):
+        return ***REMOVED***
+            "id": self.id,
+            "name": self.name,
+            "display_name": self.display_name,
+            "avatar_url": self.avatar_url.replace(".webp", ".png"),
+            "colour": self.colour
+        ***REMOVED***
--- musicbot/web_socket_server.py
+++ musicbot/web_socket_server.py
@@ -12,6 +12,7 @@ import asyncio
 from .entry import TimestampEntry
 from .simple_web_socket_server import SimpleWebSocketServer, WebSocket
 from .utils import dec_to_hex
+from .web_author import WebAuthor
 
 
 class ErrorCode:
@@ -287,34 +288,3 @@ class GieselaServer:
                     auth_client.sendMessage(json_message)
         except Exception as e:
             traceback.print_exc()
-
-
-class WebAuthor:
-
-    def __init__(self, id, name, display_name, avatar_url, colour):
-        self.id = id
-        self.name = name
-        self.display_name = display_name
-        self.avatar_url = avatar_url
-        self.colour = colour
-
-    @classmethod
-    def from_id(cls, author_id):
-        user = GieselaServer.bot.get_global_user(author_id)
-        return cls(author_id, user.name, user.display_name, user.avatar_url, dec_to_hex(user.colour.value))
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(**data)
-
-    def __str__(self):
-        return "[***REMOVED******REMOVED***/***REMOVED******REMOVED***]".format(self.id, self.name)
-
-    def to_dict(self):
-        return ***REMOVED***
-            "id": self.id,
-            "name": self.name,
-            "display_name": self.display_name,
-            "avatar_url": self.avatar_url.replace(".webp", ".png"),
-            "colour": self.colour
-        ***REMOVED***
