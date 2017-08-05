b16ea7c820a5e56cb0e3327ee0eddb22d344735c
Simon Berger <siku2@outlook.de>
2017-08-05 17:40:05 +0200

    azlyrics

--- musicbot/lyrics.py
+++ musicbot/lyrics.py
@@ -82,8 +82,7 @@ def _extract_lyrics_lyrical_nonsense(url):
 def _extract_lyrics_musixmatch(url):
     lyrics = None
 
-    headers = ***REMOVED***
-        "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"***REMOVED***
+    headers = ***REMOVED***"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"***REMOVED***
     resp = requests.get(url, headers=headers)
     content = resp.text
 
@@ -134,12 +133,29 @@ def _extract_lyrics_animelyrics(url):
     return lyrics
 
 
+def _extract_lyrics_azlyrics(url):
+    headers = ***REMOVED***"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"***REMOVED***
+    resp = requests.get(url, headers=headers)
+    bs = BeautifulSoup(resp.text, ConfigDefaults.html_parser)
+
+    center = bs.body.find("div", ***REMOVED***"class": "col-xs-12 col-lg-8 text-center"***REMOVED***)
+    lyrics = center.find("div", ***REMOVED***"class": None***REMOVED***).text
+
+    lyrics = re.sub(r"<br>", " ", lyrics)
+    lyrics = re.sub(r"<i>\W*(\w+).+(\d+).*<\/i>", "\1 \2", lyrics)
+    lyrics = re.sub(r"(?=)(\&quot\;)", "\"", lyrics)
+    lyrics = re.sub(r"<\/div>", "", lyrics)
+
+    return lyrics.strip()
+
 lyric_parsers = ***REMOVED***
     "genius.com":                  _extract_lyrics_genius,
     "www.lyricsmode.com":          _extract_lyrics_lyricsmode,
     "www.lyrical-nonsense.com":    _extract_lyrics_lyrical_nonsense,
     "www.animelyrics.com":         _extract_lyrics_animelyrics,
-    "www.musixmatch.com":          _extract_lyrics_musixmatch
+    "www.musixmatch.com":          _extract_lyrics_musixmatch,
+    "www.azlyrics.com":            _extract_lyrics_azlyrics
 ***REMOVED***
 
 # print(search_for_lyrics("Snow Fairy - Fairy Tail - English Version - Amy B"))
+# print(_extract_lyrics_azlyrics("http://www.azlyrics.com/lyrics/edsheeran/shapeofyou.html"))
