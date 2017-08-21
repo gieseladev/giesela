9c6380232da8b89f313455466f7e7b623b0f36b1
Simon Berger <siku2@outlook.de>
2017-08-21 20:47:26 +0200

    lyrics scrape some more information

--- musicbot/lyrics.py
+++ musicbot/lyrics.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 from musicbot.config import ConfigDefaults, static_config
 
 lyrics_folder = os.getcwd() + "\\" + static_config.lyrics_cache
-lyrics_version = 1
-required_version = 1
+lyrics_version = 3
+required_version = 2
 
 
 def ensure_cache_folder():
@@ -98,10 +98,8 @@ def search_for_lyrics_google(query):
                 print("Couldn't extract lyrics from ***REMOVED******REMOVED***:\n***REMOVED******REMOVED***".format(
                     display_link, traceback.format_exc()))
             if lyrics:
-                return ***REMOVED***
-                    "lyrics": lyrics,
-                    "source": display_link
-                ***REMOVED***
+                lyrics["source"] = display_link
+                return lyrics
             else:
                 print("[LYRICS] Couldn't parse these lyrics")
 
@@ -113,9 +111,17 @@ def _extract_lyrics_genius(url):
     content = resp.text
 
     bs = BeautifulSoup(content, ConfigDefaults.html_parser)
+
     lyrics_window = bs.find_all("div", ***REMOVED***"class": "lyrics"***REMOVED***)[0]
     lyrics = lyrics_window.text
-    return lyrics.strip()
+
+    title = bs.find("h1", attrs=***REMOVED***"class": "header_with_cover_art-primary_info-title"***REMOVED***).text.strip()
+
+    return ***REMOVED***
+        "url": url,
+        "title": title,
+        "lyrics": lyrics.strip()
+    ***REMOVED***
 
 
 def _extract_lyrics_lyricsmode(url):
@@ -126,7 +132,14 @@ def _extract_lyrics_lyricsmode(url):
     lyrics_window = bs.find_all(
         "p", ***REMOVED***"id": "lyrics_text", "class": "ui-annotatable"***REMOVED***)[0]
     lyrics = lyrics_window.text
-    return lyrics.strip()
+
+    title = bs.find("h1", attrs=***REMOVED***"class": "song_name fs32"***REMOVED***).text.strip()
+
+    return ***REMOVED***
+        "url": url,
+        "title": title,
+        "lyrics": lyrics.strip()
+    ***REMOVED***
 
 
 def _extract_lyrics_lyrical_nonsense(url):
@@ -139,10 +152,16 @@ def _extract_lyrics_lyrical_nonsense(url):
         bs = BeautifulSoup(content, ConfigDefaults.html_parser)
         # take the Romaji version if there is one, otherwise use the default
         # one
-        lyrics_window = bs.find_all("div", ***REMOVED***"id": "Romaji"***REMOVED***)[
-            0] or bs.find_all("div", ***REMOVED***"id": "Lyrics"***REMOVED***)[0]
+        lyrics_window = bs.find_all("div", ***REMOVED***"id": "Romaji"***REMOVED***)[0] or bs.find_all("div", ***REMOVED***"id": "Lyrics"***REMOVED***)[0]
         lyrics = lyrics_window.text
-        return lyrics.strip()
+
+        title = bs.select("div.titletext2new h3")[0].text.strip()
+
+        return ***REMOVED***
+            "url": url,
+            "title": title,
+            "lyrics": lyrics.strip()
+        ***REMOVED***
     except IndexError:
         return None
 
@@ -162,7 +181,37 @@ def _extract_lyrics_musixmatch(url):
         garbage.clear()
 
     lyrics = lyrics_window.text
-    return lyrics.strip()
+
+    title = bs.find("h1", attrs=***REMOVED***"class": "mxm-track-title__track"***REMOVED***).contents[-2].strip()
+
+    return ***REMOVED***
+        "url": url,
+        "title": title,
+        "lyrics": lyrics.strip()
+    ***REMOVED***
+
+
+def _extract_lyrics_azlyrics(url):
+    headers = ***REMOVED***"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"***REMOVED***
+    resp = requests.get(url, headers=headers)
+    bs = BeautifulSoup(resp.text, ConfigDefaults.html_parser)
+
+    center = bs.body.find("div", ***REMOVED***"class": "col-xs-12 col-lg-8 text-center"***REMOVED***)
+    lyrics = center.find("div", ***REMOVED***"class": None***REMOVED***).text
+
+    lyrics = re.sub(r"<br>", " ", lyrics)
+    lyrics = re.sub(r"<i?>\W*", "[", lyrics)
+    lyrics = re.sub(r"\W*<\/i>", "]", lyrics)
+    lyrics = re.sub(r"(?=)(\&quot\;)", "\"", lyrics)
+    lyrics = re.sub(r"<\/div>", "", lyrics)
+
+    title = center.find("h1").text.strip()[1:-8]
+
+    return ***REMOVED***
+        "url": url,
+        "title": title,
+        "lyrics": lyrics.strip()
+    ***REMOVED***
 
 
 def _extract_lyrics_animelyrics(url):
@@ -198,34 +247,23 @@ def _extract_lyrics_animelyrics(url):
         if match:
             lyrics = match.group(1).strip()
 
-    return lyrics
+    title = bs.find("td", attrs=***REMOVED***"valign": "top"***REMOVED***).find("h1").text.strip()
 
-
-def _extract_lyrics_azlyrics(url):
-    headers = ***REMOVED***"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"***REMOVED***
-    resp = requests.get(url, headers=headers)
-    bs = BeautifulSoup(resp.text, ConfigDefaults.html_parser)
-
-    center = bs.body.find("div", ***REMOVED***"class": "col-xs-12 col-lg-8 text-center"***REMOVED***)
-    lyrics = center.find("div", ***REMOVED***"class": None***REMOVED***).text
-
-    lyrics = re.sub(r"<br>", " ", lyrics)
-    lyrics = re.sub(r"<i?>\W*", "[", lyrics)
-    lyrics = re.sub(r"\W*<\/i>", "]", lyrics)
-    lyrics = re.sub(r"(?=)(\&quot\;)", "\"", lyrics)
-    lyrics = re.sub(r"<\/div>", "", lyrics)
-
-    return lyrics.strip()
+    return ***REMOVED***
+        "url": url,
+        "title": title,
+        "lyrics": lyrics.strip()
+    ***REMOVED***
 
 
 lyric_parsers = ***REMOVED***
     "genius.com":                  _extract_lyrics_genius,
     "www.lyricsmode.com":          _extract_lyrics_lyricsmode,
     "www.lyrical-nonsense.com":    _extract_lyrics_lyrical_nonsense,
-    "www.animelyrics.com":         _extract_lyrics_animelyrics,
     "www.musixmatch.com":          _extract_lyrics_musixmatch,
-    "www.azlyrics.com":            _extract_lyrics_azlyrics
+    "www.azlyrics.com":            _extract_lyrics_azlyrics,
+    "www.animelyrics.com":         _extract_lyrics_animelyrics
 ***REMOVED***
 
 # print(search_for_lyrics("Snow Fairy - Fairy Tail - English Version - Amy B"))
-# print(_extract_lyrics_azlyrics("http://www.azlyrics.com/lyrics/edsheeran/shapeofyou.html"))
+# print(_extract_lyrics_animelyrics("https://www.animelyrics.com/anime/bakuon/feelalive.htm"))
