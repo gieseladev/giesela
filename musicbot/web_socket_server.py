c0a7aa9bbd4b0fb5525df227745fa6944c0a5744
Simon Berger <siku2@outlook.de>
2017-09-02 15:56:10 +0200

    we don't need to know the update's content

--- musicbot/web_socket_server.py
+++ musicbot/web_socket_server.py
@@ -455,7 +455,7 @@ class GieselaServer:
         frame = inspect.currentframe()
         outer_frames = inspect.getouterframes(frame)
         caller = outer_frames[1]
-        print("[WEBSOCKET] Broadcasting ***REMOVED******REMOVED*** to ***REMOVED******REMOVED*** socket(s). Caused by \"***REMOVED******REMOVED***\"".format(kwargs, len(GieselaServer.authenticated_clients), caller.function))
+        print("[WEBSOCKET] Broadcasting update to ***REMOVED******REMOVED*** socket(s). Caused by \"***REMOVED******REMOVED***\"".format(len(GieselaServer.authenticated_clients), caller.function))
 
         message = ***REMOVED***
             "update": kwargs
