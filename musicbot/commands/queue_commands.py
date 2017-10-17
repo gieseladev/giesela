27c3b3443f1dc3774d772c8a45f2eed88444f070
Shikhir Arora <shikhir@thesarogroup.com>
2017-10-17 00:42:26 -0400

    Fixes move description
    See: https://trello.com/c/Hg6vTTc7/32-move-command-documentation

--- musicbot/commands/queue_commands.py
+++ musicbot/commands/queue_commands.py
@@ -757,7 +757,8 @@ class ManipulateCommands:
         ///|Usage
         `{command_prefix}move <from index> <to index>`
         ///|Explanation
-        Split a timestamp-entry into its sub-entries.
+        Moves an entry from a given position. 
+        For example, `{command_prefix}move 22 2` will move entry 22 in the queue to position 2 in the queue.
         """
 
         if from_index.isnumeric():
