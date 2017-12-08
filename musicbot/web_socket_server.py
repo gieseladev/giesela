1a089a308d570e77e6dda5bcbaa042ea1dde1343
Simon Berger <siku2@outlook.de>
2017-12-09 00:05:42 +0100

    need to keep this in mind

--- musicbot/web_socket_server.py
+++ musicbot/web_socket_server.py
@@ -295,7 +295,9 @@ class GieselaWebSocket(WebSocket):
                 entry = command_data.get("item")
                 kind = command_data.get("kind")  # entry, playlist
                 mode = command_data.get("mode", "queue")  # now, next, queue, random
-                searcher = command_data.get("searcher")
+                searcher = command_data.get("searcher")  # YoutubeSearcher, SpotifySearcher, SoundcloudSearcher
+
+                # enter async env with run_coroutine_threadsafe. Handle Spotify and then just feed the queue add_entry with the url.
 
                 self.log("playing ***REMOVED******REMOVED*** from ***REMOVED******REMOVED*** with mode ***REMOVED******REMOVED***".format(kind, searcher, mode))
 
