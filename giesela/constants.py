117772ce7a2e966d9c8d6854d8ddd12d7001855f
Simon Berger <siku2@outlook.com>
2019-03-01 17:55:56 +0100

    pumping dat version

--- giesela/constants.py
+++ giesela/constants.py
@@ -1,11 +1,5 @@
-MAIN_VERSION = "5.5.4"
+MAIN_VERSION = "5.6.0"
 SUB_VERSION = "refreshed-ll"
 VERSION = MAIN_VERSION + "_" + SUB_VERSION
 
 CONFIG_LOCATION = "data/config.yml"
-
-maj_versions = {
-    "5": "Refreshed",
-    "4": "Webiesela",
-    "3": "Giesenesis"
-}
